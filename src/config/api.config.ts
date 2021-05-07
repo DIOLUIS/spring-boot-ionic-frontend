b4e8c503e57076a0c5bfaecb329246de0431b84f
DIOLUIS <diogoparazita@gmail.com>
2021-05-07 18:37:33 -0300

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
+    baseUrl: "http://localhost:8080",
+    bucketBaseurl: "https://cursospring-diogo.s3-sa-east-1.amazonaws.com"
 }    
